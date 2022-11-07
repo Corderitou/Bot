d560183f38062e6d4797292730ab20fdc515fae4
Corderitou <chesterperry06@gmail.com>
2022-11-06 23:08:45 -0300

    a

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
 # This example requires the 'message_content' privileged intents
+import telebot
+import requests
+import threading
+from flask import Flask, request, abort
 
-import os
-import discord
-from discord.ext import commands
+app = Flask(__name__)
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        a = request.json
+        b = a["content"]
+        if b == "buy":
+            print("compra")
+        elif b == "sell": 
+            print("venta")   
+        else:
+            print("no paso nada")     
+        return 'success', 200
+    
+    else:
+        abort(400)
+
+bot = telebot.TeleBot("5777810159:AAFRLseImr310gVmCorL_ZoD7Mz6uaHHKc0")
+
+@bot.message_handler(commands=["start","test"])
+def cmd_start(message):
+    bot.send_message(-1001880307694, "hola:D") #-1001523347078
 
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
 
+def setInterval(func,time):
+    e = threading.Event()
+    while not e.wait(time):
+        func()
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
+def foo():
+    response = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+    a = response.json()
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+    priceBTC = a["price"]
+    price = int(float(priceBTC))
+    a = str(price)
+  
+    bot.send_message(-1001880307694, "BTC: "+ a)
 
+# using
+setInterval(foo,0.5)
 
-bot.run(os.environ["DISCORD_TOKEN"])
+if __name__ == "__main__":
+  print("iniciando")
+  bot.infinity_polling()
+  print("Fin")
